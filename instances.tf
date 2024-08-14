69670f1dc742b3af5eae57ff822a70b1eb0eebcc
doyindevops <adedoyindevops@gmail.com>
2024-08-14 15:44:54 +0100

    jumphost PublicIP

--- instances.tf
+++ instances.tf
@@ -26,13 +26,14 @@ resource "aws_instance" "jump_host" {
   ami           = "ami-03e722b916fa65716"
   instance_type = var.instance_type
   key_name      = var.key_name
-  subnet_id     = aws_subnet.public_subnet_2.id
+  subnet_id     = aws_subnet.public_subnet_1.id  # Move to a public subnet
   security_groups = [aws_security_group.instance_sg.id]
 
   tags = {
     Name = "Jump Host"
   }
 }
+
 
 resource "aws_instance" "bastion_host" {
   ami           = "ami-0daac31e03d86a2f5"
@@ -50,9 +51,8 @@ resource "aws_instance" "prometheus_server" {
   ami           = "ami-0daac31e03d86a2f5"  
   instance_type = "t2.micro"
   key_name      = "devops-key-pair"
-  subnet_id     = aws_subnet.private_subnet_1.id  # Specify the subnet where Prometheus should be deployed
+  subnet_id     = aws_subnet.private_subnet_1.id
 
-  # Correct the security group reference
   security_groups = [aws_security_group.prometheus_sg.id]
 
   tags = {
@@ -63,7 +63,10 @@ resource "aws_instance" "prometheus_server" {
     type        = "ssh"
     user        = "ubuntu"
     private_key = file("/home/doyin/devops-key-pair.pem")
-    host        = self.public_ip
+    host        = self.private_ip  # Use private IP
+    bastion_host = aws_instance.jump_host.public_ip  # Use the jump host as a bastion
+    bastion_user = "ubuntu"
+    bastion_private_key = file("/home/doyin/devops-key-pair.pem")
   }
 }
 
